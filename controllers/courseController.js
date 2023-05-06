c72b29f744b3b04678aed622f32373db7da68696
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-05-07 00:13:44 +0300

    courses listed

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,12 +1,30 @@
 const Course = require('../models/Course')
 
 exports.createCourse = async (req, res) => {
-    const course = await Course.create(req.body)
 
     try {
+        const course = await Course.create(req.body)
+
         res.status('201').json({
             status: 'success',
             course
+        })
+    } catch (error) {
+        res.status('400').json({
+            status: 'fail',
+            error
+        })
+    }
+
+}
+exports.getAllCourses = async (req, res) => {
+
+    try {
+        const courses = await Course.find()
+
+        res.status('200').render('course-grid-2',{
+            courses,
+            page_name:'course-grid-2'
         })
     } catch (error) {
         res.status('400').json({
