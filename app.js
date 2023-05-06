6666b75354b06788b835bb698b2e8ca034816275
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-05-06 22:58:47 +0300

    mongodb connect bug fix

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const courseRoute=require('./routes/courseRoute')
 
 const app =express()
 mongoose.connect('mongodb://localhost/smartedu-db',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false,
-    useCreateIndex:true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(()=>{
     console.log('Db connected')
 })
+.catch((error) => {
+    console.error(error);
+  });
 app.set('view engine','ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/',pageRoute)
 app.use('/courses',courseRoute)
