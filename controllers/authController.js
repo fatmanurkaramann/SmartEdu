f8eb70f0c13d8725c72c1580006aba0452ad6dce
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-05-20 19:32:51 +0300

    registwe validation with express validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const Category = require('../models/Category')
 const Course = require('../models/Course')
-
+const { validationResult } = require('express-validator');
 
 const bcrypt = require('bcrypt')
 
@@ -13,10 +13,14 @@ exports.createUser = async (req, res) => {
 
 
     } catch (error) {
-        res.status('400').json({
-            status: 'fail',
-            error
-        })
+        const errors = validationResult(req)
+        console.log(errors.array().map(e=>e.msg))
+      
+        for (let i = 0; i <errors.array().length; i++) {
+            req.flash("error", `${errors.array()[i].msg}`);
+          }
+        res.redirect('/')
+
     }
 }
 exports.loginUser = async (req, res) => {
@@ -26,9 +30,9 @@ exports.loginUser = async (req, res) => {
         const user = await User.findOne({ email })
         if (user) {
             bcrypt.compare(password, user.password, (err, same) => {
-                    // USER SESSION
-                    req.session.userId = user._id
-                    res.status(200).redirect('/user/dashboard');
+                // USER SESSION
+                req.session.userId = user._id
+                res.status(200).redirect('/user/dashboard');
             });
         }
 
@@ -49,7 +53,7 @@ exports.logoutUser = (req, res) => {
 exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({ _id: req.session.userId }).populate('courses')
     const categories = await Category.find()
-    const courses=await Course.find({user:req.session.userId})
+    const courses = await Course.find({ user: req.session.userId })
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         user,
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const pageController = require('../controllers/pageController')
 const authController = require('../controllers/authController')
 const redirectMiddleware = require('../middlewares/redirectMiddleware')
-
+const { check } = require('express-validator');
 
 const router = express.Router()
 
@@ -10,7 +10,12 @@ router.route('/').get(pageController.getHomePage)
 router.route('/about').get(pageController.getAboutPage)
 router.route('/contact').get(pageController.getContactPage)
 router.route('/contact').post(pageController.sendEmail)
-router.route('/register').post(redirectMiddleware, authController.createUser)
+router.route('/register').post([
+    check('name').notEmpty().withMessage('Please Enter Your Name'),
+    check('email').isEmail().withMessage('Please Enter a Valid Email'),
+    check('password').notEmpty().withMessage('Please Enter Your Password')
+
+],redirectMiddleware, authController.createUser)
 router.route('/login').post(redirectMiddleware, authController.loginUser)
 
 module.exports = router
